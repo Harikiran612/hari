9d6d84db42e67a0d0f04f0f42783793c0485e936
mylaptop <mylaptop@192.168.0.2>
2019-06-26 00:36:57 +0530

    added class Arraysort

---
+++ src/javaprj6/Arraysrt.java
@@ -0,0 +1,16 @@
+package javaprj6;
+
+import java.util.Arrays;
+
+public class Arraysrt {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a={77,23,44};
+		
+		Arrays.sort(a);
+		System.out.println("arr:"+Arrays.toString(a));
+
+
+}
+
+}
